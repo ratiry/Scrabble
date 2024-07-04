f319b417eab604959559ece96421f26af5ced175
ratiry <ratiry@yandex.ru>
2024-07-04 15:20:33 +0300

    made board

--- src/components/Pages/Game/Playfield/Playfield.jsx
+++ src/components/Pages/Game/Playfield/Playfield.jsx
@@ -2,9 +2,10 @@ import P from "../../../Common/Typography/P/P";
 import Cell from "./Cell/Cell";
 import classes from "./Playfield.module.scss";
 import { Board } from "../../../Helpers/Data";
+import cellClasses from "./Cell/Cell.module.scss";
 const Playfield=(props)=>{
   const cells = [...Array(15 * 15).keys()];
-  const cellsHtml=cells.map((cell,index)=>{return <div className={classes.cell}>{<Cell board={Board[index]} id={index} />}</div>})
+  const cellsHtml=cells.map((cell,index)=>{return <div className={cellClasses.cell}>{<Cell board={Board[index]} id={index} />}</div>})
   return (
     <div className={classes.playfield}>
       {cellsHtml}
